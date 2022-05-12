6b717cb33afdb55bfb54f270c182bf020f51d9ba
Maryam Gholami <gholamimaryam@gmail.com>
2022-05-12 18:28:51 +0430

    Create Parquest file

--- df.scala
+++ df.scala
@@ -1,7 +1,29 @@
 
+import org.apache.spark.sql.DataFrame
 
 
+def extractDf(fileName: String) : DataFrame = {
+  val df= spark.read.option("header","true").option("inferSchema","true").csv(fileName)
+  return df
+  }
 
-val df= spark.read.csv("NYCfiles/green_tripdata_2021-2.csv")
+def transformDf(df:DataFrame) : DataFrame={
+  val outDf= df.orderBy(asc("VendorID"))
+  return outDf
+}
 
-df.show()
+def loadDf(df:DataFrame): Boolean={
+  df.write.parquet("NYCfiles/green_tripdata_2021-2.parquet")
+  return true
+}
+
+def NYCMain():Unit={
+  val df= extractDf("NYCfiles/green_tripdata_2021-2.csv")
+  val tranfomedDf=transformDf(df)
+  for (row <- tranfomedDf.head(2)){
+    println(row)
+  }
+  val result= loadDf(tranfomedDf)
+}
+
+NYCMain()
